e392c3dd25c9fd3d041d325820703749beac52d7
mohamed600yassin-cyber <mohamed600yassin@gmail.com>
2026-05-17 13:18:26 +0200

    Add files via upload

---
+++ ai_studio_code.js
@@ -0,0 +1,8 @@
+import { createClient } from '@base44/sdk';
+
+const base44 = createClient({
+  appId: "6a084a2ef80e4539d197b735",
+  headers: {
+    "api_key": "5b05633bb7e34ba68d81ff2c6cb51481"
+  }
+});
